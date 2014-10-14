1f460c1c291a5d3dffae5489d5a7409ba7fd99f6
Alexey Petruchik <stopiccot@Alexeys-MacBook-Pro.local>
2014-10-14 11:28:31 +0300

    configure CMake build in separate folder

--- cmake_osx.sh
+++ cmake_osx.sh
@@ -1,1 +1,2 @@
-cmake -DNO_OPENCL=1 -DNO_CLEW=1 -DNO_PTEX=1 -DNO_OMP=1 -DNO_TBB=1 -DNO_CUDA=1 -DNO_MAYA=1 -G Xcode OpenSubdiv
+rm -rf build && mkdir build && cd build
+cmake -DNO_OPENCL=1 -DNO_CLEW=1 -DNO_PTEX=1 -DNO_OMP=1 -DNO_TBB=1 -DNO_CUDA=1 -DNO_MAYA=1 -G Xcode ../OpenSubdiv
