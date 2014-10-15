ba1f7eae009dd5920d253c4762bca713e648dc41
stopiccot <alexey.petruchik@gmail.com>
2014-10-15 18:37:24 +0300

    enable OpenCL for OS X

--- cmake_osx.sh
+++ cmake_osx.sh
@@ -1,2 +1,3 @@
 rm -rf build && mkdir build && cd build
-cmake -DNO_OPENCL=1 -DNO_CLEW=1 -DNO_PTEX=1 -DNO_OMP=1 -DNO_TBB=1 -DNO_CUDA=1 -DNO_MAYA=1 -G Xcode ../OpenSubdiv
+cmake -DNO_CLEW=1 -DNO_PTEX=1 -DNO_OMP=1 -DNO_TBB=1 -DNO_CUDA=1 -DNO_MAYA=1 -G Xcode ../OpenSubdiv
+open OpenSubdiv.xcodeproj/
